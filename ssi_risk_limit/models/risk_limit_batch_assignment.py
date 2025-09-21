84a4a5c80bcbbb6d68ae57268a36f21949122027
Andhitia Rama <andhitia.r@gmail.com>
2025-09-21 23:33:55 +0700

    [UPD] ssi_risk_limit
    *Penambahan queue pada batch

--- ssi_risk_limit/models/risk_limit_batch_assignment.py
+++ ssi_risk_limit/models/risk_limit_batch_assignment.py
@@ -2,7 +2,9 @@
 # Copyright 2025 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from math import ceil
+
+from odoo import _, api, fields, models
 
 from odoo.addons.ssi_decorator import ssi_decorator
 
@@ -112,6 +114,26 @@ class RiskLimitBatchAssignment(models.Model):
         readonly=True,
     )
 
+    # queue
+    queue_job_batch_id = fields.Many2one(
+        string="Queue Job Batch",
+        comodel_name="queue.job.batch",
+        readonly=True,
+        copy=False,
+    )
+
+    queue_job_ids = fields.One2many(
+        string="Queue Jobs",
+        comodel_name="queue.job",
+        related="queue_job_batch_id.job_ids",
+        store=False,
+    )
+    queue_job_batch_state = fields.Selection(
+        string="Queue Job Batch State",
+        related="queue_job_batch_id.state",
+        store=True,
+    )
+
     @api.depends("type_id")
     def _compute_allowed_partner_ids(self):
         for record in self:
@@ -129,6 +151,135 @@ class RiskLimitBatchAssignment(models.Model):
     def action_load_partner(self):
         for record in self.sudo():
             record._load_partner()
+
+    def action_confirm_assignment(self):
+        for record in self.sudo():
+            record._confirm_assignment()
+
+    def action_start_assignment(self):
+        for record in self.sudo():
+            record._start_assignment()
+
+    def action_approve_assignment(self):
+        for record in self.sudo():
+            record._approve_assignment()
+
+    def action_reject_assignment(self):
+        for record in self.sudo():
+            record._reject_assignment()
+
+    def action_cancel_assignment(self):
+        for record in self.sudo():
+            record._cancel_assignment()
+
+    def action_restart_assignment(self):
+        for record in self.sudo():
+            record._restart_assignment()
+
+    def action_finish_assignment(self):
+        for record in self.sudo():
+            result = record._finish_assignment()
+        return result
+
+    def _open_assignment(self):
+        self.ensure_one()
+        action = self.env.ref("ssi_risk_limit.risk_limit_assignment_action").read()[0]
+        action["domain"] = [("batch_id", "=", self.id)]
+        return action
+
+    def _create_job_batch(self, batch_name):
+        self.ensure_one()
+        batch = self.env["queue.job.batch"].get_new_batch(batch_name)
+        self.write(
+            {
+                "queue_job_batch_id": batch.id,
+            }
+        )
+
+    def _process_risk_limit_assignment(
+        self,
+        action_method,
+        state_filter,
+        batch_action_name,
+        split_action_name,
+        state_condition=None,
+    ):
+        self.ensure_one()
+        batch_name = f"{batch_action_name} batch risk limit assignment ID {self.id}"
+        self._create_job_batch(batch_name)
+        data_per_split = 100
+        if state_condition:
+            effected_assigments = self.risk_limit_assignment_ids.filtered(
+                state_condition
+            )
+        else:
+            effected_assigments = self.risk_limit_assignment_ids.filtered(
+                lambda r: r.state == state_filter
+            )
+        num_split = ceil(len(effected_assigments) / data_per_split)
+        for split_number in range(1, num_split + 1):
+            assignments = effected_assigments[
+                (data_per_split * split_number)
+                - data_per_split : split_number * data_per_split
+            ]
+            description = f"{split_action_name} batch {self.id}"
+            description += f" split {split_number}"
+            getattr(
+                assignments.with_context(job_batch=self.queue_job_batch_id).with_delay(
+                    description=_(description)
+                ),
+                action_method,
+            )()
+            self.queue_job_batch_id.enqueue()
+
+    def _confirm_assignment(self):
+        self._process_risk_limit_assignment(
+            action_method="action_confirm",
+            state_filter="draft",
+            batch_action_name="Confirm",
+            split_action_name="Confirm",
+        )
+
+    def _finish_assignment(self):
+        self._process_risk_limit_assignment(
+            action_method="action_done",
+            state_filter="open",
+            batch_action_name="Finish",
+            split_action_name="Finish",
+        )
+
+    def _approve_assignment(self):
+        self._process_risk_limit_assignment(
+            action_method="action_approve_approval",
+            state_filter="confirm",
+            batch_action_name="Approve",
+            split_action_name="Approve",
+        )
+
+    def _reject_assignment(self):
+        self._process_risk_limit_assignment(
+            action_method="action_reject_approval",
+            state_filter="confirm",
+            batch_action_name="Reject",
+            split_action_name="Reject",
+        )
+
+    def _cancel_assignment(self):
+        self._process_risk_limit_assignment(
+            action_method="action_cancel",
+            state_filter=None,
+            batch_action_name="Cancel",
+            split_action_name="Cancel",
+            state_condition=lambda r: r.state != "cancel",
+        )
+
+    def _restart_assignment(self):
+        self._process_risk_limit_assignment(
+            action_method="action_restart",
+            state_filter="cancel",
+            batch_action_name="Restart",
+            split_action_name="Restart",
+        )
 
     def _load_partner(self):
         self.ensure_one()
